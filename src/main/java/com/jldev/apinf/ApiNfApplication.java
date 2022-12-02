c410de0bea4512670abb6299b30242180d0ad82f
Jeferson Labout <70985458+Jeferson-Labout@users.noreply.github.com>
2022-12-02 15:51:49 -0300

    first commit

---
+++ src/main/java/com/jldev/apinf/ApiNfApplication.java
@@ -0,0 +1,13 @@
+package com.jldev.apinf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiNfApplication.class, args);
+	}
+
+}
